1edfaf3a87ac40faaaddc2b85a2ee80a01eff027
rainrisa <risarainly@gmail.com>
2025-02-11 19:58:57 +0700

    chore: simplify force subs .env
    While leaving the old FORCE_GROUP_IDS and FORCE_CHANNEL_IDS compatible with the new FORCE_SUB_IDS, to prevent any breaking changes

--- src/services/env.ts
+++ src/services/env.ts
@@ -1,18 +1,25 @@
 import "dotenv/config";
-import { bool, cleanEnv, num, str } from "envalid";
+import { bool, cleanEnv, makeValidator, num, str } from "envalid";
 
+const numArr = makeValidator<number[]>((input: string) => {
+  const coerced = input.split(" ").map(Number);
+  return coerced;
+});
 const env = cleanEnv(process.env, {
   TELEGRAM_BOT_TOKEN: str(),
   DB_CHANNEL_ID: num(),
   DEVELOPMENT: bool({ default: undefined }),
   WEBHOOK_DOMAIN: str({ default: undefined }),
   PORT: num({ default: 8080 }),
-  FORCE_CHANNEL_IDS: str({ default: undefined }),
-  FORCE_GROUP_IDS: str({ default: undefined }),
+  FORCE_CHANNEL_IDS: numArr({ default: undefined }),
+  FORCE_GROUP_IDS: numArr({ default: undefined }),
+  FORCE_SUB_IDS: numArr({ default: [] }),
   ADMIN_IDS: str(),
   DATABASE_URL: str({ default: undefined }),
   NO_FORWARD: bool({ default: undefined }),
   BROADCAST_LOG_DELAY: num({ default: undefined }),
 });
+env.FORCE_SUB_IDS.push(...(env.FORCE_GROUP_IDS || []));
+env.FORCE_SUB_IDS.push(...(env.FORCE_CHANNEL_IDS || []));
 
 export default env;
